294beccdc606d2ad5e304f8c00722af0d6d6846c
Richard Reade <ricreade@live.com>
2016-04-21 22:18:00 -0700

    CommandUtils method buildout.

---
+++ RatEngine/Engine/Command/CommandUtils.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using RatEngine.DataModel;
+
+namespace RatEngine.Engine.Command
+{
+    /// <summary>
+    /// Static class for with methods for the evaluation of a command string.
+    /// </summary>
+    public static class CommandUtils
+    {
+        public static GameElement GetCommandTarget(string Command)
+        {
+            return null;
+        }
+
+        public static GameElement GetCommandUseObject(string Command)
+        {
+            return null;
+        }
+
+        public static string GetCommandMessage(string Command)
+        {
+            return null;
+        }
+    }
+}
